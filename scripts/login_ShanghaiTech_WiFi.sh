cd72c2f32d228ee29a5e4f879e4abfcb0901c48d
lyhokia <lyhokia@gmail.com>
2021-02-05 13:16:31 +0800

    Add scripts

---
+++ scripts/login_ShanghaiTech_WiFi.sh
@@ -0,0 +1,4 @@
+curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST -v --data \
+	"userName={username}&password={password}" \
+	https://controller.shanghaitech.edu.cn:8445/PortalServer//Webauth/webAuthAction\!login.action
+
