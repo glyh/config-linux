d6ee1ffa1c99e4dbd16262bdb907af67fb37e5f7
lyhokia <lyhokia@gmail.com>
2021-02-06 14:55:00 +0800

    Add recipe.yml

--- config.clj
+++ config.clj
@@ -4,39 +4,24 @@
          '[clojure.java.shell :as shell]
          '[clojure.string :as str])
 
-(defn expand-home [s]
+(defn expand-dir [s]
   (if (.startsWith s "~")
     (str/replace-first s "~" (System/getProperty "user.home"))
     s))
 
-(defn mkdir-p [dir]
-  (let [result (shell/sh "mkdir" "-p" dir)]
-    (if (zero? (:exit result))
-      ()
-      (list (str "Failed(" (:exit result) "): " (:err result))))))
+(defn stow-one [package target]
+  (let [result (shell/sh "stow" "-d" "./dot-files" "-t" target "-S" package)] 
+    (locking *out*
+      (println 
+       (str "Result " (:exit result) ": " 
+            (if (empty? (:err result)) "Success" (:err result)))))))
 
-(defn cp-r [source dest]
-  (let [result (shell/sh "cp" "-r" source dest)]
-    (if (zero? (:exit result))
-      ()
-      (list (str "Failed(" (:exit result) "): " (:err result))))))
+(defn stow-all []
+  (println "Stowing...")
+  (->> (slurp "./dot-files/targets.edn")
+       (edn/read-string)
+       (pmap (fn [x] (stow-one (:dir x)  
+                               (expand-dir (:target x))))))
+  (println "Stowed!"))
 
-(defn copy-statics [source dest]
-  (concat (mkdir-p dest) (cp-r source dest)))
-
-(defn copy-all-statics []
-  ; Solve static files
-  (let [potential-errors (->> (slurp "./static/mappings.edn")
-              (edn/read-string)
-              (pmap (fn [x] 
-                      (copy-statics (str "./static/" (:source x) "/.") (expand-home (:dest x)))))
-              (flatten))]
-    (if (empty? potential-errors) 
-      "Successfully copied all static files"
-      potential-errors)) 
- )
-
-(defn -main []
-  (println (copy-all-statics)))
-
-(-main)
+(stow-all)
--- static/to-config/.SpaceVim.d/autoload/boot.vim
+++
@@ -1,7 +0,0 @@
-function! boot#before() abort
-  " For: deoplete.nvim
-  "let g:deoplete#enable_at_startup = 1
-endfunction
-
-function! boot#after() abort
-endfunction
